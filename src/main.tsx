6775df232f8d31bac8b1d3ae428a1e0d92037bd8
Shailesh Thakur <thakurshaileshkumar507@gmail.com>
2025-07-22 21:38:01 +0530

    imported vercel speedInsight

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,8 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { injectSpeedInsights } from '@vercel/speed-insights'  
 
-createRoot(document.getElementById("root")!).render(<App />);
+injectSpeedInsights() 
+
+createRoot(document.getElementById("root")!).render(<App />)
